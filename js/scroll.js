8357ed565ec4460b614c3658c22c77c96ec8d5e0
RANDOM cosmos <srijanwhat2@gmail.com>
2021-07-17 15:06:15 +0530

    Initilise repo

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+var prevScrollpos = 0;
+$("nav").css("top", "0");
+
+$(window).scroll(() => {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    $("nav").css("top", "0");
+  } else {
+    $("nav").css("top", "-100px");
+  }
+  
+  prevScrollpos = currentScrollPos;
+});
+
+document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
